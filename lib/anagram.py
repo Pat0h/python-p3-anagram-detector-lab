bbcb1a0d41d3d496fbadada55c83df707a947574
Pat0h <patrickngatia14@gmail.com>
2023-12-06 16:29:23 +0300

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = sorted(word.lower())
+
+    def match(self, listy=[]):
+        match = [x for x in listy if self.word == sorted(x.lower())]
+        return match if match else []
+
+
